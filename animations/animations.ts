5a79f1e94ee76397457fcfdc9b5ca46729adfb1e
Todd Mpeli <toddowenmpeli@rocketmail.com>
2025-11-19 12:28:44 +0300

    Created animation folder.

---
+++ animations/animations.ts
@@ -0,0 +1,126 @@
+/* -----------------------------------------------------------------------------
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX Import XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+----------------------------------------------------------------------------- */
+
+import * as IAnimation from "@/animations/types/index";
+
+/* -----------------------------------------------------------------------------
+XXXXXXXXXXXXXXXXXXXXXXXXX FRAMER-MOTION ANIMATIONS XXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+----------------------------------------------------------------------------- */
+
+/* 
+Custom reusable Animation Properties/variables
+
+This is the default framer-motion scrollYProgress content reveal.
+ Provides When in view should the content be revealed */
+export const offsetStart: number = 0.9;
+export const offsetFinish: number = 0.5;
+
+export const initial: IAnimation.IInitial = {
+	y: 0,
+	opacity: 0,
+};
+export const initialTwo: IAnimation.IInitialTwo = {
+	y: 0,
+	opacity: 0,
+};
+export const initialThree: IAnimation.IInitial = {
+	y: 90,
+	opacity: 0,
+};
+export const fadeIn: IAnimation.IFadeIn = {
+	opacity: 1,
+	transition: {
+		delay: 0.5,
+		duration: 0.75,
+		ease: [0, 0, 0.2, 1],
+	},
+};
+export const fadeInTwo: IAnimation.IFadeInTwo = {
+	y: 0,
+	opacity: 1,
+	transition: {
+		duration: 1,
+		delay: 0.5,
+		ease: [0.42, 0, 0.58, 1],
+	},
+};
+export const fadeInUp: IAnimation.IFadeInUp = {
+	y: 0,
+	opacity: 1,
+	transition: {
+		delay: 0.5,
+		duration: 0.5,
+		ease: [0.42, 0, 0.58, 1],
+	},
+};
+export const stagger: IAnimation.IStagger = {
+	initial: {
+		opacity: 0,
+		y: 0,
+	},
+	animate: {
+		opacity: 1,
+		y: 0,
+		transition: {
+			delay: 0.1,
+			duration: 0.75,
+			ease: [0.42, 0, 0.58, 1],
+		},
+	},
+};
+export const arrayLoopStaggerChildren: IAnimation.IArrayLoopStaggerChildren = {
+	initial: {
+		opacity: 0,
+		y: 0,
+	},
+	animate: (keys: number) => ({
+		opacity: 1,
+		y: 0,
+		transition: {
+			delay: 0.1 * keys,
+			duration: 0.75,
+			ease: [0.42, 0, 0.58, 1],
+		},
+	}),
+};
+export const navigationMenuStaggerChildren: IAnimation.IArrayLoopStaggerChildren = {
+	initial: {
+		opacity: 0,
+		y: 0,
+	},
+	animate: (keys: number) => ({
+		opacity: 1,
+		y: 0,
+		transition: {
+			delay: 0.25 * keys,
+			duration: 0.5,
+			ease: [0.42, 0, 0.58, 1],
+		},
+	}),
+};
+
+// Slide In Direction (Horizontal)
+export const slideInRightInitial: IAnimation.ISlideInRightInitial = {
+	y: 0,
+	x: 200,
+	opacity: 0,
+};
+export const slideInLeftInitial: IAnimation.ISlideInLeftInitial = {
+	y: 0,
+	x: -200,
+	opacity: 0,
+};
+export const slideInRightFinish: IAnimation.ISlideInRightFinish = {
+	y: 0,
+	x: 0,
+	opacity: 1,
+	transition: {
+		delay: 0.5,
+		duration: 0.75,
+		ease: [0.42, 0, 0.58, 1],
+		staggerChildren: 0.1,
+	},
+};
+
+export default fadeInUp;
---
+++ animations/types/index.ts
@@ -0,0 +1,270 @@
+/* -----------------------------------------------------------------------------
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX Import XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+----------------------------------------------------------------------------- */
+
+import { JSX } from "react";
+import {Variants} from "framer-motion";
+
+/* -----------------------------------------------------------------------------
+XXXXXXXXXXXXXXXXXXXXXXXXX FRAMER-MOTION ANIMATIONS XXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+----------------------------------------------------------------------------- */
+
+// 3D Tilt animation effect
+export type IUseTiltEffect = {
+	rotateX: number;
+	rotateY: number;
+	translateX: number;
+	translateY: number;
+};
+
+// Text Effect Reveal
+export type IPresetType = "blur" | "shake" | "scale" | "fade" | "slide";
+
+export type ITextEffectProps = {
+	children: string;
+	per?: "word" | "char";
+	as?: keyof JSX.IntrinsicElements;
+	variants?: {
+		container?: Variants;
+		item?: Variants;
+	};
+	className?: string;
+	preset?: IPresetType;
+};
+
+// Title & Paragraph Text Sliced Slant Mask Animation
+export type ITextSlicedSlantMaskAnimation = {
+	initial: {
+		y: string;
+	};
+	enter: (i: number) => {
+		y: string;
+		transition: {
+			duration: number;
+			ease: number[];
+			delay: number;
+		};
+	};
+};
+
+// Text Wrap Scroll Effect Reveal Animation
+export type IIntroTextAnimation = {
+	title: string;
+	endColor?: string;
+	className?: string;
+	startColor?: string;
+};
+
+// Text Blur Scroll Effect Reveal Animation
+export type ITextBlurScrollEffect = {
+	content: string;
+	className?: string;
+};
+
+// Work Experience Modal Scale Animation
+export type IWorkExperienceModalScaleAnimation = {
+	initial: {
+		scale: number;
+		x: string;
+		y: string;
+	};
+	enter: {
+		scale: number;
+		x: string;
+		y: string;
+		transition: {
+			duration: number;
+			ease: number[];
+		};
+	};
+	closed: {
+		scale: number;
+		x: string;
+		y: string;
+		transition: {
+			duration: number;
+			ease: number[];
+		};
+	};
+};
+
+// WhileInView
+export type IInitial = {
+	y: number;
+	opacity: number;
+};
+export type IInitialTwo = {
+	y: number;
+	opacity: number;
+};
+export type IFadeIn = {
+	opacity: number;
+	transition: {
+		duration: number;
+		delay: number;
+		ease: [number, number, number, number];
+	};
+};
+export type IFadeInTwo = {
+	y: number;
+	opacity: number;
+	transition: {
+		delay: number;
+		duration: number;
+		ease: [number, number, number, number];
+	};
+};
+export type IFadeInUp = {
+	y: number;
+	opacity: number;
+	transition: {
+		delay: number;
+		duration: number;
+		ease: [number, number, number, number];
+	};
+};
+export type IStagger = {
+	initial: {
+		opacity: number;
+		y: number;
+	};
+	animate: {
+		opacity: number;
+		y: number;
+		transition: {
+			delay: number;
+			duration: number;
+			ease: [number, number, number, number];
+		};
+	};
+};
+export type IArrayLoopStaggerChildren = {
+	initial: {
+		opacity: number;
+		y: number;
+	};
+	animate: (keys: number) => {
+		opacity: number;
+		y: number;
+		transition: {
+			delay: number;
+			duration: number;
+			ease: [number, number, number, number];
+		};
+	};
+};
+export type ISlideInRightInitial = {
+	y: number;
+	x: number;
+	opacity: number;
+};
+export type ISlideInLeftInitial = {
+	y: number;
+	x: number;
+	opacity: number;
+};
+export type ISlideInRightFinish = {
+	x: number;
+	y: 0;
+	opacity: number;
+	transition: {
+		delay: number;
+		duration: number;
+		ease: [number, number, number, number];
+		staggerChildren: number;
+	};
+};
+
+// Work Experience Animation
+export type ITextMiddleImageReveal = {
+	initial: {
+		width: number;
+	};
+	open: {
+		width: string;
+		transition: {
+			duration: number;
+			ease: number[];
+		};
+	};
+	closed: {
+		width: number;
+	};
+};
+
+// Scroll Based Velocity
+export type IVelocityScrollProps = {
+	text: string;
+	className?: string;
+	displayFour: boolean;
+	defaultVelocity?: number;
+};
+export type IParallaxProps = {
+	children: string;
+	baseVelocity: number;
+	className?: string;
+};
+
+// Text Typing Animation
+export type ITextTypingAnimation = {
+	text: string;
+	color?: string | number;
+	display?: string | boolean;
+	opacity?: number;
+	duration?: number;
+	className?: string;
+};
+
+// Title, Paragraph Text & Content Mask Animation
+export type ITitleTextMaskAnimation = {
+	text: string;
+	className?: string;
+	styleTextColor?: string;
+	isPageTitle?: boolean;
+};
+export type IContentMaskAnimation = {
+	children: React.ReactNode;
+};
+export type IParagraphTextMaskAnimation = {
+	text: string;
+	className?: string;
+	styleTextColor?: string;
+};
+
+// Text Scroll Button
+export type ITextScrollButton = {
+	className?: string;
+	classNameTwo?: string;
+	classNameThree?: string;
+	styleTextColor?: string;
+	children: React.ReactNode;
+	styleButtonLineColor?: string;
+};
+
+export type ITransforms = {
+	x: number;
+	y: number;
+	rotationZ: number;
+}[];
+export type IDisperse = {
+	open: (i: number) => {
+		x: string;
+		y: string;
+		rotateZ: number;
+		transition: {
+			duration: number;
+			ease: number[];
+		};
+		zIndex: number;
+	};
+	closed: {
+		x: number;
+		y: number;
+		rotateZ: number;
+		transition: {
+			duration: number;
+			ease: number[];
+		};
+		zIndex: number;
+	};
+};
