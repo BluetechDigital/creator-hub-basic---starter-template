ba9cab08efd6160d7688e2137ddb25a8b28ec80d
Todd Mpeli <toddowenmpeli@rocketmail.com>
2025-11-20 20:17:17 +0300

    Updated component styling and removed next .js boiler template code.

--- components/CMS/TitleParagraph/TitleParagraph.tsx
+++ components/CMS/TitleParagraph/TitleParagraph.tsx
@@ -43,35 +43,29 @@ const TitleParagraph: FC<ITitleParagraph> = memo(({
 	paragraph,
 	displayParagraph,
 }) => {
+
 	return (
 		<ScrollYProgressReveal className={styles.titleParagraph}>
-			<motion.div
+			<motion.h2
 				initial={initial}
 				whileInView={fadeInUp}
 				viewport={{once: true}}
-				className={styles.container}
+				className={title ? styles.title : "hidden"}
 			>
-				<motion.h2
-					initial={initial}
-					whileInView={fadeInUp}
-					viewport={{once: true}}
-					className={title ? styles.title : "hidden"}
-				>
-					{title}
-				</motion.h2>
-				<Paragraph
-					fadeIn={false}
-					content={paragraph}
-					offsetStart={offsetStart}
-					offsetFinish={offsetFinish}
-					className={
-						paragraph
-							? styles.paragraph +
-							` ${displayParagraph ? "text-center lg:text-center" : "text-center lg:text-left"}`
-							: "hidden"
-					}
-				/>
-			</motion.div>
+				{title}
+			</motion.h2>
+			<Paragraph
+				fadeIn={false}
+				content={paragraph}
+				offsetStart={offsetStart}
+				offsetFinish={offsetFinish}
+				className={
+					paragraph
+						? styles.paragraph +
+						` ${displayParagraph ? "text-center lg:text-center" : "text-center lg:text-left"}`
+						: "hidden"
+				}
+			/>
 		</ScrollYProgressReveal>
 	);
 });
