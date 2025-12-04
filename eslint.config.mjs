1e37d75324695a91b8f1cfcc94b5acc16b0005e2
Todd <toddowenmpeli02@gmail.com>
2025-12-04 11:24:21 +0300

    Minor comment changes.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,7 @@
+/* -----------------------------------------------------------------------------
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX Import XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+----------------------------------------------------------------------------- */
+
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
